32b684ba3b4afd0f792175323507da4f602ed1d8
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-07 13:10:10 +0530

    second_commit

--- intro.py
+++ intro.py
@@ -1,18 +1,45 @@
-#popular quick sort algorithm
+# popular quick sort algorithm
 
-q= lambda l : q([x for x in l[1:] if x <= l[0]]) + [l[0]] + q([x for x in l if x > l[0]]) if l else []
+def q(l): return q([x for x in l[1:] if x <= l[0]]) + \
+    [l[0]] + q([x for x in l if x > l[0]]) if l else []
 
-print(q([3,1,42,32]))
 
+print(q([3, 1, 42, 32]))
+
+# list comprehension
 employees = {'Alice': 100000,
              'Bob': 99817,
              'Carol': 122908,
              'frank': 88123,
              'Eve': 93121}
 
-top_earners =[]
+top_earners = []
 
 for key, val in employees.items():
-    if val >= 100000: 
-        top_earners.append((key,val))
+    if val >= 100000:
+        top_earners.append((key, val))
 print(top_earners)
+
+top_earners1 = [(key, value)
+                for key, value in employees.items() if value >= 10000]
+print(top_earners)
+
+# reading file and adding
+filename = 'intro.py'
+f = open(filename)
+lines = []
+for line in f:
+    print(line.strip())
+    lines.append(line.strip())
+
+print(lines)
+
+print([line.strip() for line in open('intro.py')])
+
+txt = ['last month my manager',
+       'removed my admin access  anonymous now I am doing some installation.',
+       'for mach1ml project anonymous so i need admin access again.']
+
+# map function and ternary operator
+mark = map(lambda s: (True, s) if 'anonymous' in s else (False, s), txt)
+print(mark)
