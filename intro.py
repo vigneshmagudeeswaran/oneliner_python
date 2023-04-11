00f73b0f2348b4c2933e6b89ac7a1f1712a1f444
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-11 18:43:19 +0530

    LambdaMApTernaryOperator

--- intro.py
+++ intro.py
@@ -43,3 +43,15 @@ txt = ['last month my manager',
 # map function and ternary operator
 mark = map(lambda s: (True, s) if 'anonymous' in s else (False, s), txt)
 print(mark)
+
+
+letters_amazon = '''
+we spent several years building our own database engine, Amazon Aurora, 
+a fully-managed MySQL and PostgreSQL-compatible service with the
+same or better durability and availability as the commercial engines, 
+but at one-tenth of the cost. We were not surprised when this worked.
+'''
+
+find = lambda x, q: x[x.find(q)-18:x.find(q)+18] if q in x else -1
+
+print(find(letters_amazon,'SQL'))
