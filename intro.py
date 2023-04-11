5b073b3af86aaa5425c5d87950e7f8155c792810
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-11 19:54:20 +0530

    LambdaMApTernaryOperator

--- intro.py
+++ intro.py
@@ -52,6 +52,18 @@ same or better durability and availability as the commercial engines,
 but at one-tenth of the cost. We were not surprised when this worked.
 '''
 
-find = lambda x, q: x[x.find(q)-18:x.find(q)+18] if q in x else -1
 
-print(find(letters_amazon,'SQL'))
+def find(x, q): return x[x.find(q)-18:x.find(q)+18] if q in x else -1
+
+
+print(find(letters_amazon, 'SQL'))
+
+# SAMPLING A 2D Python list
+
+price = [[9.9, 9.8, 9.8, 9.4, 9.5, 9.7],
+         [9.5, 9.4, 9.4, 9.3, 9.2, 9.1],
+         [8.4, 7.9, 7.9, 8.1, 8.0, 8.0],
+         [7.1, 5.9, 4.8, 4.8, 4.7, 3.9]]
+
+sample = [line[::2] for line in price]
+print(sample)
